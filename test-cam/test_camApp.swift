ff85b91a9ab12c9ede8e6253a003da06ab48e39c
rythmn1111 <rythmnmagnani0@gmail.com>
2025-11-09 00:05:23 +0530

    Initial Commit

---
+++ test-cam/test_camApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_camApp.swift
+//  test-cam
+//
+//  Created by rythmn on 09/11/25.
+//
+
+import SwiftUI
+
+@main
+struct test_camApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
